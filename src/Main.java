63a50a9d84bab8bcce309c1151273abe1fc65c3b
SvOlga <sviridova-olga.iccf@yandex.ru>
2020-05-21 21:04:09 +0300

    floating numbers

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import static java.math.BigDecimal.*;
+
 public class Main {
     public static void main(String[] args) {
-        int amount = 1199;                          // сумма пополнения
-        int balance = 100;                          // баланс клиента до пополнения
-        int bonus = amount > 1000 ? amount / 100 : 0;
-        balance = balance + amount + bonus;         //Итоговый баланс
+        BigDecimal amount = valueOf(1000.01);       //сумма пополнения
+        BigDecimal balance = valueOf(100);          //баланс клиента до пополнения
+        BigDecimal bonus = null;
+        BigDecimal percent = valueOf(100);
+        if (amount.compareTo(valueOf(1000)) == 1) {     //вычисление бонуса
+            bonus = amount.divide(percent, 0, RoundingMode.DOWN);
+        } else bonus = valueOf(0);
+
+        balance = balance.add(amount);
+        balance = balance.add(bonus);                  //баланс после пополнения
 
         System.out.println("Бонус = " + bonus);
     }
